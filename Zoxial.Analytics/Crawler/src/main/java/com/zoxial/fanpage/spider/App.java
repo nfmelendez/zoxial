e592df45bc65babaac9ffa6320c75ddfb663e525
Nicolás Melendez <nfmelendez@gmail.com>
2012-06-25 22:42:28 -0300

    * add configuration library * remove my acess token :)

--- Zoxial.Analytics/Crawler/src/main/java/com/zoxial/fanpage/spider/App.java
+++ Zoxial.Analytics/Crawler/src/main/java/com/zoxial/fanpage/spider/App.java
@@ -18,47 +18,46 @@ import akka.util.Duration;
 import ar.com.blog.melendez.asyncrestfb.Main;
 import ar.com.blog.melendez.asyncrestfb.messages.Fetch;
 
-/**
- * Hello world!
- * 
- */
+
 public class App {
 	public static void main(String[] args) {
 		ActorSystem system = Main.init();
-		// Metadata persistence actor
-		Connection conn = null;
-		ResultSet query = null;
-		try {
-			conn = FanPage7DaysFetcher.datasource.getConnection();
-			ResultSet count = conn.createStatement().executeQuery("SELECT count(*) AS total FROM charts;"); 
-			count.next();
-			int totalRouter = count.getInt("total");
+
 		final ActorRef resourceFetcher = system.actorOf(new Props(
 				new UntypedActorFactory() {
 					public UntypedActor create() {
 						return new FanPage7DaysFetcher();
 					}
-				}).withRouter(new RoundRobinRouter(totalRouter)), FanPage7DaysFetcher.class
-				.getName());
+				}).withRouter(new RoundRobinRouter(5)),
+				FanPage7DaysFetcher.class.getName());
 
-		String token = "AAAFdWOPfKvEBADiIi1ZB7bLnxpnKBjKGlYn1wVIw1gBHlYYXbZBzp2p0jPy9FrWITiaIuezb5KHkoRFZB0fCMcb5862QrhKs9jE4AvvGgZDZD";
-		
-			query = conn.createStatement().executeQuery("SELECT facebook_page_id FROM charts;");
-			while(query.next()){
+		String token = ConfigResource.INSTANCE.getString("facebook.token");
+	
+		// Metadata persistence actor
+		Connection conn = null;
+		ResultSet query = null;
+		try {
+			conn = FanPage7DaysFetcher.datasource.getConnection();
+			query = conn.createStatement().executeQuery(
+					"SELECT facebook_page_id FROM charts;");
+			while (query.next()) {
 				String facebook_page_id = query.getString("facebook_page_id");
 				Properties p = new Properties();
 				p.put("pageToFetch", facebook_page_id);
 				p.put("token", token);
-				system.scheduler().schedule(Duration.create(0, TimeUnit.MINUTES),
-						Duration.create(1, TimeUnit.MINUTES), resourceFetcher, new Fetch(p));
+				system.scheduler().schedule(
+						Duration.create(0, TimeUnit.MINUTES),
+						Duration.create(1, TimeUnit.MINUTES), resourceFetcher,
+						new Fetch(p));
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			DbUtils.closeQuietly(query);
 			DbUtils.closeQuietly(conn);
 		}
-				
+
 	}
+	
 }
---
+++ Zoxial.Analytics/Crawler/src/main/java/com/zoxial/fanpage/spider/ConfigResource.java
@@ -0,0 +1,28 @@
+package com.zoxial.fanpage.spider;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.log4j.Logger;
+
+public class ConfigResource {
+
+	private static final Logger log = Logger.getLogger(ConfigResource.class);
+	public static Configuration INSTANCE;
+
+	private ConfigResource() {
+	}
+
+	static {
+		String filename = "config/config.properties";
+
+		try {
+			INSTANCE = new PropertiesConfiguration(filename);
+			log.info("Configuration was succefully created.");
+		} catch (ConfigurationException e1) {
+			log.error("Something wrong happen with configurations", e1);
+		}
+	}
+
+
+}
