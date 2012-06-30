816468a9e8eb461a81c90e282104243c73fb3fb6
Nicolás Melendez <nfmelendez@gmail.com>
2012-06-30 16:34:14 -0300

    documentation

--- Zoxial.Analytics/Crawler/src/main/java/com/zoxial/fanpage/spider/ConfigResource.java
+++ Zoxial.Analytics/Crawler/src/main/java/com/zoxial/fanpage/spider/ConfigResource.java
@@ -5,14 +5,34 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.Logger;
 
+/**
+ * Represents the config resource, it loads the configuration one time when the
+ * classloader load this class and then properties must be read and never write
+ * into this resource. Actually the files path is in config/config.properties
+ * and each property of the config file is documented inside it. This resource
+ * is to load once, and read forever. That make it threadsafe. Change the
+ * properties after it was load is forgiven, but not enforced. If you change a
+ * property after it loaded, then there could exist a race condition. It is an
+ * eager Singleton.
+ * 
+ * @author @nfmelendez nfmelendez@gmail.com
+ * 
+ */
 public class ConfigResource {
 
+	/** Logger */
 	private static final Logger log = Logger.getLogger(ConfigResource.class);
+
+	/** The only instance of ConfigResouce in all the jvm, Cannot be null */
 	public static Configuration INSTANCE;
 
+	/** Private constructor to disable creation from outside */
 	private ConfigResource() {
 	}
 
+	/**
+	 * Method executed by the clasloader since it is an eager Singleton.
+	 */
 	static {
 		String filename = "config/config.properties";
 
@@ -23,6 +43,5 @@ public class ConfigResource {
 			log.error("Something wrong happen with configurations", e1);
 		}
 	}
-
 
 }
