48cf4480533a3aa8e7da96bfacc216c9f986f624
Nicolás Melendez <nfmelendez@gmail.com>
2012-07-01 13:43:29 -0300

    enforce creation of log directory in deploy

--- Zoxial.Analytics/Crawler/start.sh
+++ Zoxial.Analytics/Crawler/start.sh
@@ -1,3 +1,4 @@
+mkdir -p log
 mvn clean install
 nohup mvn exec:java > /dev/null &
 echo $! > app.pid
--- Zoxial.Analytics/Fanpage-UI/start.sh
+++ Zoxial.Analytics/Fanpage-UI/start.sh
@@ -1,3 +1,4 @@
+mkdir -p log
 mvn clean install
 nohup mvn jetty:run > log/logging.log &
 echo $! > app.pid
