56e0d41925e740017e3214b0051d0b982ff58d65
Nicolás Melendez <nfmelendez@gmail.com>
2012-07-03 23:50:48 -0300

    fix bug with maven setting.xml path

--- Zoxial.Analytics/Crawler/start.sh
+++ Zoxial.Analytics/Crawler/start.sh
@@ -1,5 +1,5 @@
 mkdir -p log
-mvn clean install
+mvn clean $1 install
 nohup mvn $1 exec:java > /dev/null  2>&1 &
 echo $! > app.pid
 
--- Zoxial.Analytics/Fanpage-UI/start.sh
+++ Zoxial.Analytics/Fanpage-UI/start.sh
@@ -1,5 +1,5 @@
 mkdir -p log
-mvn clean install
+mvn $1 clean install
 nohup mvn $1 jetty:run > log/logging.log  2>&1 &
 echo $! > app.pid
 
